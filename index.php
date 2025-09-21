c5a4ad896129e87cebbf8eaf252040af70b800c1
EmanuelFernandoms <emanuelfernandoms.gmail.com>
2025-09-21 20:35:41 -0300

    change: favicon laranja

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" href="./assets/icons/favicon.svg" type="image/svg+xml">
+    <link rel="icon" href="./assets/icons/favicon-laranja.svg" type="image/svg+xml">
     <link rel="stylesheet" href="./app/styles/style.index.css">
     <title>PlayOn</title>
 </head>
