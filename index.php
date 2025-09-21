07d8e2ec307df424c7e44f855bb5675bda57ce0e
EmanuelFernandoms <emanuelfernandoms.gmail.com>
2025-09-21 20:30:04 -0300

    add: tela de login

--- index.php
+++ index.php
@@ -3,10 +3,33 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="./assets/icons/favicon.svg" type="image/svg+xml">
     <link rel="stylesheet" href="./app/styles/style.index.css">
     <title>Document</title>
 </head>
 <body>
-    <p>a</p>
+    <nav>
+        <img src="./assets/icons/playon vermelho.svg" alt="">
+    </nav>
+    <div class="modal_login">
+        <div class="cabecalho_login">
+            <h2>LOGIN</h2>
+        </div>
+        <div class="campos_login">
+            <div>
+                <label for="email_login">Login</label>
+                <input type="email" name="email_login" id="email_login" class="input_default">
+            </div>
+
+            <div>
+                <label for="senha_login">Senha</label>
+                <input type="password" name="senha_login" id="senha_login" class="input_default">
+            </div>
+        </div>
+        <div class="modal_footer">
+            <a href="#" class="botao_laranja" id="login">ENTRAR</a>
+            <a href="#" class="link" id="possuo_conta">Já Possuo Conta</a>
+        </div>
+    </div>
 </body>
 </html>
